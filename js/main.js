a27aa6ad06a93bfedf0d8c956fa6aca60585c77b
Alonesa <alenakrasnov@mail.ru>
2021-02-02 03:31:18 +0200

    made function to ger random number

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let getRandomNumber = function (min, max, decimalPoints) {
+  let findRandom = Math.random() * (max - min) + min;
+  return +findRandom.toFixed(decimalPoints);
+};
+
+console.log(getRandomNumber(3, 7, 0));
+
+/*
+Нашли рандомное число
+1. https://learn.javascript.ru/task/random-int-min-max
+
+Число с заданым количеством символов после запятой + перевод его в численный формат
+2.https://learn.javascript.ru/number#netochnye-vychisleniya
+*/

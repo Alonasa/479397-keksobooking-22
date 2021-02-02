9c87001e8ca9ebeafe1987967c031a9c7d52c9fe
Alonesa <alenakrasnov@mail.ru>
2021-02-02 03:51:51 +0200

    fix for failed test

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ console.log(getRandomNumber(3, 7, 0));
 Нашли рандомное число
 1. https://learn.javascript.ru/task/random-int-min-max
 
+
 Число с заданым количеством символов после запятой + перевод его в численный формат
 2.https://learn.javascript.ru/number#netochnye-vychisleniya
 */

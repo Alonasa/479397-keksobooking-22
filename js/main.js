c10112e8be88aeb69411c48855a81698852ee3d9
Alonesa <alenakrasnov@mail.ru>
2021-02-06 00:17:34 +0200

    generate property objects

--- js/main.js
+++ js/main.js
@@ -1,15 +1,85 @@
-let getRandomNumber = function (min, max, decimalPoints) {
-  let findRandom = Math.random() * (max - min) + min;
+'use strict';
+const QUANTITY = 10;
+const MIN_ROOMS = 1;
+const MAX_ROOMS = 5;
+const LINK = 'img/avatars/user0';
+const Y_MIN = 139.70000;
+const Y_MAX = 139.80000;
+const X_MIN = 35.65000;
+const X_MAX = 35.70000;
+const TYPES_OF_RENT = [
+  'palace',
+  'flat',
+  'house',
+  'bungalo',
+];
+const CHECK_HOURS = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+const FEATURES_LIST = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+const PHOTOS_LIST = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+];
+
+const getRandomNumber = function (min, max, decimalPoints) {
+  const findRandom = Math.random() * (max - min) + min;
   return +findRandom.toFixed(decimalPoints);
 };
 
-getRandomNumber(3, 7, 0);
+const getRandomArrayItem = function(arr) {
+  return arr[Math.floor(Math.random() * arr.length)]
+}
 
-/*
-Нашли рандомное число
-1. https://learn.javascript.ru/task/random-int-min-max
+const offersList = [];
+
+const getOfferInitialData = function () {
+  return {
+    author: {
+      avatar: `${LINK}${getRandomNumber(1, 8, 0)}.png`,
+    },
+
+    offer: {
+      title: 'Great apartment without toilet. Pack of diapers for free',
+      address: `${X_MIN}, ${Y_MIN}`,
+      price: getRandomNumber(1000, 100000),
+      type: getRandomArrayItem(TYPES_OF_RENT),
+      rooms: getRandomNumber(MIN_ROOMS, MAX_ROOMS),
+      guests: getRandomNumber(1, 3),
+      checkin: getRandomArrayItem(CHECK_HOURS),
+      checkout: getRandomArrayItem(CHECK_HOURS),
+      features: FEATURES_LIST,
+      description: 'С моим знанием js о Токио только мечтать🤦‍♀️🤦‍♀️🤦‍♀️‍',
+      photos: PHOTOS_LIST,
+    },
+
+    location: {
+      x: getRandomNumber(X_MIN, X_MAX, 5),
+      y: getRandomNumber(Y_MIN, Y_MAX, 5),
+    },
+  }
+}
 
 
-Число с заданым количеством символов после запятой + перевод его в численный формат
-2.https://learn.javascript.ru/number#netochnye-vychisleniya
-*/
+const generatePropertiesData = function() {
+  for(let i = 0; i < QUANTITY; i++) {
+    offersList.push(getOfferInitialData(i));
+  }
+}
+
+generatePropertiesData();
+
+
+
+
+

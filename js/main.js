4c00b3023b03d2115cfd20ee8b990e65c18da072
Alonesa <alenakrasnov@mail.ru>
2021-03-06 01:06:03 +0200

    delete gen code for advertisiment

--- js/main.js
+++ js/main.js
@@ -2,9 +2,5 @@ import './const.js';
 import './utils.js';
 import './data.js';
 //prettier-ignore
-import { generateOffer, mapCanvas, similarListFragment }
-  from './generate-simmilar-elements.js';
+import  './generate-simmilar-elements.js';
 import './form-user-input-matching.js';
-
-generateOffer();
-mapCanvas.appendChild(similarListFragment);
